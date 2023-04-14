7cef9e9269d2367baed45e4aca217bdcc8f20513
Thomas L <thomas.lombard@interhaptics.com>
2023-04-14 20:54:26 +0200

    Empty project

---
+++ Source/Rizume/Rizume.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Rizume.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Rizume, "Rizume" );
---
+++ Source/Rizume/RizumeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RizumeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RIZUME_API ARizumeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
